7f3879c03eda9ba1a668effe429ba4f6f69223fb
Stephen Kelly <steveire@gmail.com>
2022-01-18 21:52:01 +0000

    Rename macro for KTextTemplate

--- cmake/modules/KF6TextTemplateMacros.cmake
+++ cmake/modules/KF6TextTemplateMacros.cmake
@@ -1,7 +1,7 @@
 
 include(CMakeParseArguments)
 
-macro(grantlee_adjust_plugin_name pluginname)
+macro(ktexttemplate_adjust_plugin_name pluginname)
   set_target_properties(${pluginname}
     PROPERTIES PREFIX ""
     LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/grantlee/${KTextTemplate_VERSION_MAJOR}.${KTextTemplate_VERSION_MINOR}"
